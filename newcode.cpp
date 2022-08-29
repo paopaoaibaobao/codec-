bb5a409ed5485295462414d0133d4f546d99d4c6
tangxinming <965633082@qq.com>
2022-08-30 01:27:15 +0800

    good

---
+++ newcode.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    cout << sqrt(6.25) << endl;
+    cout << "hello,world" << endl;
+    system("pause");
+    return 0;
+}
